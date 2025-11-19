29b45a64c8934e29966c25203c125c7a009208a6
jlemosz <jonatan.lemos@unity3d.com>
2025-11-19 17:20:19 -0500

    Trying new script version.

--- Assets/Editor/BuildAutomation.cs
+++ Assets/Editor/BuildAutomation.cs
@@ -1,62 +1,42 @@
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
-using System.Collections.Generic;
 using System.IO;
 
 public static class BuildAutomation
 {
-    private static Queue<EditorBuildSettingsScene> sceneQueue;
-    
+    // This method can be called from a build script or the command line.
+    [MenuItem("Tools/Disable Auto Baking For Build Scenes")]
     public static void DisableAutoBaking()
     {
-        Debug.Log("Queueing build scenes to disable auto lightmap baking.");
+        Debug.Log("Starting to disable auto lightmap baking for all build scenes.");
 
-        sceneQueue = new Queue<EditorBuildSettingsScene>();
-
-        // Populate the queue with all enabled scenes from Build Settings.
         foreach (var scene in EditorBuildSettings.scenes)
         {
-            if (scene.enabled && File.Exists(scene.path) && !scene.path.Contains("Packages/"))
+            if (!scene.enabled || !File.Exists(scene.path) || scene.path.Contains("Packages/"))
             {
-                sceneQueue.Enqueue(scene);
+                continue;
+            }
+
+            EditorSceneManager.OpenScene(scene.path);
+            Debug.Log($"Opened scene: {scene.path}");
+
+            // In batch mode, we must manually queue an update to ensure the editor
+            // processes the scene load and any resulting asset imports.
+            EditorApplication.QueuePlayerLoopUpdate();
+
+            if (Lightmapping.TryGetLightingSettings(out LightingSettings lightingSettings))
+            {
+                lightingSettings.autoGenerate = false;
+                Debug.Log($"Set 'autoGenerate' to false for scene: {scene.path}");
+                EditorSceneManager.SaveOpenScenes();
+            }
+            else
+            {
+                Debug.LogWarning($"Could not retrieve LightingSettings for {scene.path}. The setting may not have been changed.");
             }
         }
 
-        // Register the update method to process the queue.
-        EditorApplication.update += ProcessSceneQueue;
-    }
-
-    private static void ProcessSceneQueue()
-    {
-        // If the queue is empty, we are done.
-        if (sceneQueue.Count == 0)
-        {
-            Debug.Log("Finished disabling auto lightmap baking for all scenes.");
-            // Unregister the update method to stop processing.
-            EditorApplication.update -= ProcessSceneQueue;
-            return;
-        }
-
-        // Dequeue the next scene and open it.
-        var sceneToProcess = sceneQueue.Dequeue();
-        Debug.Log($"Opening scene: {sceneToProcess.path}");
-        EditorSceneManager.OpenScene(sceneToProcess.path);
-
-        // Attempt to get the lighting settings.
-        if (Lightmapping.TryGetLightingSettings(out LightingSettings lightingSettings))
-        {
-            // Disable the auto-generate lighting setting.
-            lightingSettings.autoGenerate = false;
-            Debug.Log($"Set 'autoGenerate' to false for scene: {sceneToProcess.path}");
-
-            // Save the changes to the scene file.
-            EditorSceneManager.SaveOpenScenes();
-        }
-        else
-        {
-            // This can happen if the settings asset is not immediately available.
-            Debug.LogWarning($"Could not retrieve LightingSettings immediately for {sceneToProcess.path}. The setting may not have been changed.");
-        }
+        Debug.Log("Finished disabling auto lightmap baking.");
     }
 }
